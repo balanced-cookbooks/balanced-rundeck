75f0a2f26f81f4a5406b410f66e5b11c58f6fc48
Marshall Jones <marshall@balancedpayments.com>
2014-04-03 18:19:37 -0700

    move example jobs into test cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -40,24 +40,3 @@ run_context.resource_collection.each do |r|
     end
   end
 end
-
-# Create projects
-rundeck_project 'balanced' do
-  rundeck_node_source_file 'balanced'
-
-  rundeck_job 'echo' do
-    source 'balanced_echo.yml'
-  end
-
-  rundeck_job 'invoicing' do
-    source 'balanced_invoicing.yml'
-  end
-end
-
-rundeck_project 'precog' do
-  rundeck_node_source_file 'precog'
-
-  rundeck_job 'stats' do
-    source 'precog_stats.yml'
-  end
-end
