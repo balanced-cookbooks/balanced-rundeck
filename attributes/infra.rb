f330397887fbbfc54b9b3f8d5d04263e2960071e
Marshall Jones <marshall@balancedpayments.com>
2014-04-18 08:42:06 -0700

    db backup changes * specify host name if required * include chef_environment in backup name

--- attributes/infra.rb
+++ attributes/infra.rb
@@ -1,1 +1,2 @@
 default['postgres']['archiver'] = nil
+default['balanced-rundeck']['jobs']['db']['specify_host'] = false
--- recipes/infra.rb
+++ recipes/infra.rb
@@ -4,13 +4,22 @@ rundeck_project 'infra-db' do
     limit 1
   end
 
+  if node['balanced-rundeck']['jobs']['db']['specify_host']
+    host = node['postgres']['archiver']
+  else
+    host = nil
+  end
+
   %w(balanced precog justice).each do |db_name|
     rundeck_job "backup #{db_name}" do
       source 'jobs/db/backup.yml.erb'
       options(
           :db_name => db_name,
           :user => db_name,
-          :host => node['postgres']['archiver'],
+          # HACK: currently the prod db is running on old architecture. we
+          # specify the host for this combination because we're not using
+          # localhost to backup the prod db, it's another server.
+          :host => host,
           :failure_recipient => node['balanced-rundeck']['jobs']['failure_recipient'],
           :failure_notify_url => node['balanced-rundeck']['jobs']['failure_notify_url'],
           :chef_environment => node.chef_environment,
